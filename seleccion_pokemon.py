339d0c35127aa359a8e6e3437f5c5ca135c56aee
Raquel De la Rosa <78809531+raqueldelaross@users.noreply.github.com>
2021-10-19 13:12:47 -0600

    Update seleccion_pokemon.py

--- seleccion_pokemon.py
+++ seleccion_pokemon.py
@@ -1,135 +1,134 @@
-import os
-from random import randint #librearia para generar numeros aleatorios
-clear = lambda: os.system('cls') #limpiar pantalla
+from random import randint
+from random import choice
 
-def inicial(poke_incial):
+class Seleccion_pokemon:
+    def __init__(self, pokemon):
+        self.pokemon = pokemon
+
+    def inicial(self):
         nivel = 5
-        if poke_inicial == 1:
-            #DATOS
+        if self.pokemon == 1:
+            #puntos de salud
+            vi = randint(1, 20)
             EB = 45
             ataque = 49
             defensa = 49
-            a_especial = 65 #valor ataque especial
-            d_especial = 65 #valor defensa especial
             vel = 45
-            #puntos de salud
-            vi = randint(1, 45)
             ps = ((vi + 2 * EB)* (nivel/100))+10+nivel
             #puntos de ataque
-            vi2 = randint(1, 49)
+            vi2 = randint(1, 20)
             atq = ((vi2 + 2 * ataque)*(nivel/100))+5
             #puntos de defensa
-            vi3 = randint(1, 49)
+            vi3 = randint(1, 20)
             defe = ((vi3 + 2 * defensa)*(nivel/100))+5
             #puntos de ataque especial
-            vi4 = randint(1, 65)
+            vi4 = randint(1, 20)
             atq_especial = ((vi4 + 2 * ataque)*(nivel/100))+5
             #puntos de defensa especial
-            vi5 = randint(1, 65)
+            vi5 = randint(1, 20)
             def_especial = ((vi5 + 2 * defensa)*(nivel/100))+5
             #velocidad
-            vi6 = randint(1, 45)
+            vi6 = randint(1, 20)
             velocidad = ((vi6 + 2 * vel)*(nivel/100))+5
-            
+            # movimientos de BULBASAUR
+            movimientos_bulbasaur = ['Scratch', 'Growl', 'Leech seed', 'Vine whip', 'Posion powder', 'Sleep powder', 'Take down', 'Razor leaf', 'Sweet scent', 'Growth', 'Double-edge', 'Worry seed', 'Synthesis', 'Seed Bomb']
+            movimiento1 = choice(movimientos_bulbasaur)
+            movimiento2 = choice(movimientos_bulbasaur)
 
             print('Su pokemon es: BULBASAUR')
             apodo = input('Ingrese apodo: ')
             print(f"Nivel-----------: {nivel} ")
-            print('Movimientos-----: ')
-            print('<< Datos de combate >>')
+            print(f"Movimientos-----: {movimiento1}, {movimiento2}")
+            print('Datos de combate. ')
             print(f"Puntos de salud-: {ps} ")
             print(f"Ataque----------: {atq}")
             print(f"Defensa---------: {defe}")
             print(f"Ataque especial-: {atq_especial}")
             print(f"Defensa especial: {def_especial}")
             print(f"Velocidad-------: {velocidad}")
+            input('Presione cualquier tecla para continuar...')
 
-        if poke_inicial == 2:
-            #DATOS
+        elif self.pokemon == 2:
+            #puntos de salud
+            vi = randint(1, 20)
             EB = 39
             ataque = 52
             defensa = 43
-            a_especial = 60 #valor ataque especial
-            d_especial = 50 #valor defensa especial
             vel = 65
-            #puntos de salud
-            vi = randint(1, 39)
             ps = ((vi + 2 * EB)* (nivel/100))+10+nivel
             #puntos de ataque
-            vi2 = randint(1, 52)
+            vi2 = randint(1, 20)
             atq = ((vi2 + 2 * ataque)*(nivel/100))+5
             #puntos de defensa
-            vi3 = randint(1, 43)
+            vi3 = randint(1, 20)
             defe = ((vi3 + 2 * defensa)*(nivel/100))+5
             #puntos de ataque especial
-            vi4 = randint(1, 60)
-            atq_especial = ((vi4 + 2 * a_especial)*(nivel/100))+5
+            vi4 = randint(1, 20)
+            atq_especial = ((vi4 + 2 * ataque)*(nivel/100))+5
             #puntos de defensa especial
-            vi5 = randint(1, 50)
-            def_especial = ((vi5 + 2 * d_especial)*(nivel/100))+5
+            vi5 = randint(1, 20)
+            def_especial = ((vi5 + 2 * defensa)*(nivel/100))+5
             #velocidad
-            vi6 = randint(1, 65)
+            vi6 = randint(1, 20)
             velocidad = ((vi6 + 2 * vel)*(nivel/100))+5
-            
+            # movimientos de CHARMANDER
+            movimientos_charmander = ['Scratch', 'Growl', 'Ember', 'Smokescreen', 'Dragon rage', 'Scary face', 'Fire fang', 'Flame brust', 'Slash', 'Flamethrower', 'Fire spin', 'Inferno']
+            movimiento1 = choice(movimientos_charmander)
+            movimiento2 = choice(movimientos_charmander)
 
             print('Su pokemon es: CHARMANDER')
             apodo = input('Ingrese apodo: ')
             print(f"Nivel-----------: {nivel} ")
-            print('Movimientos-----: ')
-            print('<< Datos de combate >>')
+            print(f"Movimientos-----: {movimiento1}, {movimiento2}")
+            print('Datos de combate. ')
             print(f"Puntos de salud-: {ps} ")
             print(f"Ataque----------: {atq}")
             print(f"Defensa---------: {defe}")
             print(f"Ataque especial-: {atq_especial}")
             print(f"Defensa especial: {def_especial}")
             print(f"Velocidad-------: {velocidad}")
+            input('Presione cualquier tecla para continuar...')
 
-        if poke_inicial == 3:
-            #DATOS
+        elif self.pokemon == 3:
+            #puntos de salud
+            vi = randint(1, 20)
             EB = 44
             ataque = 48
             defensa = 65
-            a_especial = 50 #ataque especial
-            d_especial = 64 #defensa especial
             vel = 43
-            #puntos de salud
-            vi = randint(1, 44) #valor individual aleatorio
             ps = ((vi + 2 * EB)* (nivel/100))+10+nivel
             #puntos de ataque
-            vi2 = randint(1, 48)
+            vi2 = randint(1, 20)
             atq = ((vi2 + 2 * ataque)*(nivel/100))+5
             #puntos de defensa
-            vi3 = randint(1, 65)
+            vi3 = randint(1, 20)
             defe = ((vi3 + 2 * defensa)*(nivel/100))+5
             #puntos de ataque especial
-            vi4 = randint(1, 50)
-            atq_especial = ((vi4 + 2 * a_especial)*(nivel/100))+5
+            vi4 = randint(1, 20)
+            atq_especial = ((vi4 + 2 * ataque)*(nivel/100))+5
             #puntos de defensa especial
-            vi5 = randint(1, 64)
-            def_especial = ((vi5 + 2 * d_especial)*(nivel/100))+5
+            vi5 = randint(1, 20)
+            def_especial = ((vi5 + 2 * defensa)*(nivel/100))+5
             #velocidad
-            vi6 = randint(1, 43)
+            vi6 = randint(1, 20)
             velocidad = ((vi6 + 2 * vel)*(nivel/100))+5
-            
+            # movimientos de SQUIRTLE
+            movimientos_squirtle = ['Trackle', 'Tail whip', 'Water Gun', 'Whitdraw', 'Bubble', 'Bite', 'Rapid spin', 'Protect', 'Water pulse', 'Aqua tail', 'Skull bash', 'Iron defense', 'Rain dance', 'Hydro pump']
+            movimiento1 = choice(movimientos_squirtle)
+            movimiento2 = choice(movimientos_squirtle)            
 
             print('Su pokemon es: SQUIRTLE')
             apodo = input('Ingrese apodo: ')
             print(f"Nivel-----------: {nivel} ")
-            print('Movimientos-----: ')
-            print('<< Datos de combate >> ')
+            print(f"Movimientos-----: {movimiento1}, {movimiento2}")
+            print('Datos de combate. ')
             print(f"Puntos de salud-: {ps} ")
             print(f"Ataque----------: {atq}")
             print(f"Defensa---------: {defe}")
             print(f"Ataque especial-: {atq_especial}")
             print(f"Defensa especial: {def_especial}")
             print(f"Velocidad-------: {velocidad}")
-
-#Bloque principal:
-print(' BIENVENIDO A POKEMON ROJO. ')
-print('INICIO DE JUEGO... ')
-print('Pokemon Inicial Disponibles: ')
-print('1 - BULBASAUR. \n2 - CHARMANDER. \n3 - SQUIRTLE.')
-poke_inicial = int(input('Seleccione un pokemon inicial: '))
-
-clear()
-print(inicial(poke_inicial))
+            input('Presione cualquier tecla para continuar...')
+        
+        else:
+            print('Esta opcion no esta disponible')
