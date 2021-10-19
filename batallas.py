5068598687930ffbf55625dbb6371b0ed5a1029b
Raquel De la Rosa <78809531+raqueldelaross@users.noreply.github.com>
2021-10-19 13:09:21 -0600

    Create batallas.py

---
+++ batallas.py
@@ -0,0 +1,27 @@
+class Batallas:
+    def __init__(self, seleccion):
+        self.seleccion = seleccion
+
+    def pokemon_salvaje():
+        pass
+
+    def atacar():
+        pass
+
+    def capturar():
+        pass
+
+    def curar():
+        pass
+
+    def huir():
+        pass
+
+    def ganar():
+        pass
+
+    def perder():
+        pass
+
+    def __str__():
+        pass
